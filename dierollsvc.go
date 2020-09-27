38e19a1c87440e86c488e63b9c4a3df9c46ae8a9
Tom Sasser <tom_sasser@cable.comcast.com>
2020-09-27 23:35:02 +0300

    Added tests

--- dierollsvc.go
+++ dierollsvc.go
@@ -6,9 +6,28 @@ import (
 	"github.com/tsasser05/dieroll"
 )
 
-func Dieroll() {
-	fmt.Println("dieroll service!")
+type Roll struct {
+	Roll int `json:"roll"`
+}
+
+// Dieroll rolls 4d6 for a stat
+func Example() {
+	dieroll.Init()
+	fmt.Println("dieroll 4d6 for a stat!")
 	stat := dieroll.RollStat()
 	fmt.Printf("stat = %d", stat)
 
 }
+
+func Init() {
+	dieroll.Init()
+}
+
+/* Endpoints:
+ *
+ *  Roll(max int, num int) (returns either total of 1 die or total of all dice rolled)
+ *	RollStat() int
+ *	RollDice() []int (returns a list containing the result for each roll)
+ *	RandomKey(m map[string]string) string  (returns a random key in a map)
+ *
+ */
